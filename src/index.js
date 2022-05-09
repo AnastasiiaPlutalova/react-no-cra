e39562576f9e4355e6186f113bd59ca5b8900372
AnastasiiaPlutalova <Anastasiya.n11@gmail.com>
2022-05-09 11:25:16 +0400

    CountClicks without jsx

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('react no cra');
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+const CountClicks = () => {
+    const [clicks, setClicks] = useState(0);
+
+    return React.createElement(
+        'div',
+        null, 
+        [
+            React.createElement(Button, {children: 'Click', onClick: () => setClicks(clicks + 1)}),
+            React.createElement(Text, {text: `clicks: ${clicks}`}),
+        ]
+    );
+};
+
+const Button = (props) => {
+    return React.createElement('button', {...props});
+};
+
+const Text = (props) => {
+    return React.createElement('span', null, `${props.text}`); 
+}
+
+const App = (props) => {
+    return React.createElement('div', null, props.children);
+};
+
+ReactDOM.render(
+    React.createElement(App, null, React.createElement(CountClicks)),
+    document.querySelector('#root')
+);
