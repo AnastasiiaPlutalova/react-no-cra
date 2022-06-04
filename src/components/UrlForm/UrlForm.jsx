872e29e4c093399f0229a9dc5a92ad26eec5dbbb
AnastasiiaPlutalova <Anastasiya.n11@gmail.com>
2022-06-04 14:16:28 +0400

    set up UrlForm

--- src/components/UrlForm/UrlForm.jsx
+++ src/components/UrlForm/UrlForm.jsx
@@ -3,29 +3,42 @@ import { TextInput } from '../../units';
 
 import './UrlForm.scss';
 
-const UrlForm = () => {
-    const [url, setUrl] = useState();
-    const [error, setError] = useState();
+const r =
+  /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+function UrlForm() {
+  const [url, setUrl] = useState('');
+  const [error, setError] = useState();
 
-    const handleUrlChange = () => {
-        console.log(1);
+  const handleUrlChange = (event) => {
+    const { value } = event.target;
+    setUrl(value);
+
+    if (value.search(r) === -1) {
+      setError('Please enter correct URL');
+    } else {
+      setError(null);
     }
+  };
 
-    return (
-        <form className='url-form'>
-            <TextInput
-                name="url"
-                label="URL"
-                placeholder="URL"
-                value={url}
-                onChange={handleUrlChange}
-                error={error}
-            />
-            <div>
-                <input type="submit" value="Shorten URL" />
-            </div>
-        </form>
-    );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form className="url-form" onSubmit={handleSubmit}>
+      <TextInput
+        name="url"
+        label="URL"
+        placeholder="URL"
+        value={url}
+        onChange={handleUrlChange}
+        error={error}
+      />
+      <div>
+        <input type="submit" value="Shorten URL" />
+      </div>
+    </form>
+  );
 }
 
 export default UrlForm;
--- src/units/TextInput/TextInput.jsx
+++ src/units/TextInput/TextInput.jsx
@@ -4,37 +4,48 @@ import PropTypes from 'prop-types';
 
 import './TextInput.scss';
 
-const TextInput = ({type, name, label, placeholder, value, onChange, error, classNames}) => {
-    return (
-        <div className="text-input">
-            <label className="text-input__label" htmlFor={name}>{label}</label>
-            <input
-                id={name}
-                type={type}
-                name={name}
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
-                className={cn(['text-input__input', {error: !!error}, classNames])}
-            />
-            {error && <div className='text-input__error'>{error}</div>}
-        </div>
-    );
+function TextInput({
+  type,
+  name,
+  label,
+  placeholder,
+  value,
+  onChange,
+  error,
+  classNames,
+}) {
+  return (
+    <div className="text-input">
+      <label className="text-input__label" htmlFor={name}>
+        {label}
+      </label>
+      <input
+        id={name}
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className={cn(['text-input__input', { error: !!error }, classNames])}
+      />
+      {error && <div className="text-input__error">{error}</div>}
+    </div>
+  );
 }
 
 TextInput.propTypes = {
-    type: PropTypes.string,
-    name: PropTypes.string,
-    label: PropTypes.string,
-    placeholder: PropTypes.string,
-    value: PropTypes.string,
-    onChange: PropTypes.func,
-    error: PropTypes.string,
-    classNames: PropTypes.string
+  type: PropTypes.string,
+  name: PropTypes.string,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  error: PropTypes.string,
+  classNames: PropTypes.string,
 };
 
 TextInput.defaultProps = {
-    type: 'text',
+  type: 'text',
 };
 
 export default TextInput;
