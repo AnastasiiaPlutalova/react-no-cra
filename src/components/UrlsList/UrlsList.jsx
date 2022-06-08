ec4c6069f2d6eedb04b6b33903797edfa2297e9f
AnastasiiaPlutalova <Anastasiya.n11@gmail.com>
2022-06-08 16:05:00 +0400

    add copy url functionality

--- src/components/UrlsList/UrlsList.jsx
+++ src/components/UrlsList/UrlsList.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCopy } from '@fortawesome/free-regular-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 import './UrlsList.scss';
 
 function UrlsList({ urls }) {
+  const [copiedUrl, setCopiedUrl] = useState();
+
+  const handleCopyClick = async (textToCopy) => {
+    await navigator.clipboard.writeText(textToCopy);
+    setCopiedUrl(textToCopy);
+    setTimeout(() => {
+      setCopiedUrl(null);
+    }, 1500);
+  };
+
   const urlItem = (item) => {
     const { text = 'url', url } = item;
     return (
       <li className="urls-list__item" key={item.url}>
-        <FontAwesomeIcon className='urls-list__icon' icon={faCopy} />
+        <FontAwesomeIcon
+          onClick={() => handleCopyClick(item.url)}
+          className="urls-list__icon"
+          icon={item.url === copiedUrl ? faCheck : faCopy}
+        />
         {text}:{' '}
         <a href={url} title={text}>
           {url}
