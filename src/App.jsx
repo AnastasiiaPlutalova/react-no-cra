382a6851763ca59d9ff1227dc2fefe656318b25f
AnastasiiaPlutalova <Anastasiya.n11@gmail.com>
2022-06-13 20:09:41 +0400

    add ErrorBoundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Header } from './components';
+import { Header, ErrorBoundary } from './components';
 import { FrontPage, RedirectPage, StatisticPage } from './pages';
 
 import './common/styles/styles.scss';
@@ -9,11 +9,13 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<FrontPage />} />
-        <Route path="/statistic" element={<StatisticPage />} />
-        <Route path="/:hash" element={<RedirectPage />} />
-      </Routes>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<FrontPage />} />
+          <Route path="/statistic" element={<StatisticPage />} />
+          <Route path="/:hash" element={<RedirectPage />} />
+        </Routes>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as UrlForm } from './UrlForm/UrlForm';
 export { default as UrlsList } from './UrlsList/UrlsList';
 export { default as DeleteUrl } from './DeleteUrl/DeleteUrl';
 export { default as Header } from './Header/Header';
+export { default as ErrorBoundary } from './ErrorBoundary/ErrorBoundary';
