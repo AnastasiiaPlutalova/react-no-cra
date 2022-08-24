8c1695ea8184a4e024fb0733ddc5d7e9a72297bf
AnastasiiaPlutalova <Anastasiya.n11@gmail.com>
2022-08-24 15:51:25 +0200

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { SunSkirts } from './Pages';
+
 function App() {
-  return 'app';
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SunSkirts />} />
+        <Route path="/sun-skirts" element={<SunSkirts />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,26 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    devServer: {
-        port: 3000,
-    },
-    resolve: {
-        extensions: ['.js','.jsx'] 
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: './public/index.html'}),
-    ],
-    module: {
-        rules: [
-          {
-            test: /\.s[ac]ss$/i,
-            use: [
-              "style-loader",
-              "css-loader",
-              "sass-loader",
-            ],
-          },
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use:  'babel-loader'
-          }
-        ],
+  mode: 'development',
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
-}
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
